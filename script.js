561b71e733c097f5da2b00ccbc93ac65eca3760f
Mantas1983 <linkevicius83@gmail.com>
2022-12-19 21:32:26 +0000

    cikklas cikle

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let result = '';
 const lenght = 7;
 for (let i = 1; i < lenght; i++) {
     for( let j = 0; j < i; j++) {
-        result+='*';
+        result += "*";
     }
-    result= '\n'; 
+    result += '\n'; 
 }
 console.log(result);
-    
-    
+ 
+// for(let i=1; i <= 6; i++){
+//     console.log('*'.repeat(i)); 
+// }
