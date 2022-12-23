d56aa26a3e6821a7c97843acff625e99d7c0ded5
Mantas1983 <linkevicius83@gmail.com>
2022-12-23 13:19:52 +0000

    zvigzduciu piesimas

--- ciklai-nd.js
+++ ciklai-nd.js
@@ -76,3 +76,20 @@
 // }
 // console.log(data);
 // return(data);
+
+// zvaigzduciu piesimas
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
