60796eb83609660a4731100d63c9c8da41cf81d2
Mantas1983 <linkevicius83@gmail.com>
2022-12-23 12:59:23 +0000

    ciklai namu darbai

--- ciklai-nd.js
+++ ciklai-nd.js
@@ -43,9 +43,36 @@
 // }
 
 // Заполните массив цифрами от 5 до 10 включительно. Помните, что элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
-const arrayOFNumbers = [];
-for (let i = 5; i <= 10; i++) {
-    arrayOFNumbers[i - 5] = i;
-}
-console.log(arrayOFNumbers);
-return arrayOFNumbers;
+// const arrayOFNumbers = [];
+// for (let i = 5; i <= 10; i++) {
+//     arrayOFNumbers[i - 5] = i;
+// }
+// console.log(arrayOFNumbers);
+// return arrayOFNumbers;
+
+// Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let i = 0; i < arr.length; i++){
+//     result[i] = arr[i];
+// }
+// console.log(result);
+// return(result);
+
+// Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for(let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = i * 2;
+//     }
+//     else if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`; 
+//     }
+// }
+// console.log(data);
+// return(data);
